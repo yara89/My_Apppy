bc92a81eb8c326c4ca9fefa24246618b750e17e6
yara89 <60362934+yara89@users.noreply.github.com>
2021-02-22 16:23:37 +0100

    Update __init__.py
    started tutorial 11 of flask blog, attempted to add map (API not read correctly)

--- tabadol/__init__.py
+++ tabadol/__init__.py
@@ -1,25 +1,45 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_googlemaps import GoogleMaps, Map, icons
+from flask_mail import Mail
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
-# app.config.from_object('tabadol.default_settings')
 # app.config.from_envvar('YOURAPPLICATION_SETTINGS')
-# api_key = ' '  change this to your api key
-#  api key from Google API Console (https://console.cloud.google.com/apis/)
-# GoogleMaps(app, key=api_key)  set api_key
+
+# api key from Google API Console (https://console.cloud.google.com/apis/)
+
+app.config['GOOGLEMAPS_KEY'] = os.environ.get('API_KEY')
+
+# app.config.from_object('tabadol.default_settings')
+# api_key = ' '  change this to your api key "AIzaSyBCnM5Y7jFzbc98onY43LS821QZ0_GMOMg"
+# GoogleMaps(app, key=api_key)  set api_key map_key=AIzaSyBCnM5Y7jFzbc98onY43LS821QZ0_GMOMg
 # devices_data = {}  dict to store data of devices
 # devices_location = {} # dict to store coordinates of devices
 # use sqlalchemy or something to store things in database
 
+GoogleMaps(app)
+api_key = os.environ.get('API_KEY')
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
+
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
+
+# Mail
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
+app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
+mail = Mail(app)
+
 
 from tabadol import routes
