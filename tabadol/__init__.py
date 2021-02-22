8831d79ac5206cb440d8f52e9e8033b3a6a57715
yara89 <60362934+yara89@users.noreply.github.com>
2021-02-22 16:52:52 +0100

    Update __init__.py
    api key

--- tabadol/__init__.py
+++ tabadol/__init__.py
@@ -8,7 +8,7 @@ from flask_mail import Mail
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+app.config['SECRET_KEY'] = os.environ.get('API_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
 # app.config.from_envvar('YOURAPPLICATION_SETTINGS')
