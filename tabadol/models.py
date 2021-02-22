cfda1ebf51f7ea901d735304b9632125b57e5634
yara89 <60362934+yara89@users.noreply.github.com>
2021-02-22 16:24:01 +0100

    Update models.py
    started tutorial 11 of flask blog, attempted to add map (API not read correctly)

--- tabadol/models.py
+++ tabadol/models.py
@@ -1,6 +1,7 @@
 import enum
 from datetime import datetime
-from tabadol import db, login_manager
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from tabadol import db, login_manager, app
 from flask_login import UserMixin
 from sqlalchemy import func
 
@@ -31,6 +32,19 @@ class User(db.Model, UserMixin):
     # Relationship between offers and the users
     user_offers = db.relationship(
         'Post', back_populates='user', order_by="Post.id", lazy=True)
+
+    def get_reset_token(self, expires_sec=1800):
+        s = Serializer(app.config['SECRET_KEY'])
+        return s.dumps({'user_id': self.id}).decode('utf-8')
+
+    @staticmethod
+    def verify_reset_token(token):
+        s = Serializer(app.config['SECRET_KEY'])
+        try:
+            user_id = s.loads(token)['user_id']
+        except:
+            return None
+        return User.query.get(user_id)
 
     def __repr__(self):
         return f"User('{self.username}', '{self.email}', '{self.image_file}', type:{self.user_type})"
