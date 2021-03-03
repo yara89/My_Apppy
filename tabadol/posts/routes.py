c8fb0729c07577534079f3b8b2c7f4b187d69f6b
yara89 <60362934+yara89@users.noreply.github.com>
2021-03-03 09:50:42 +0100

    Update routes.py
    connect to Spatialite terminal error (no such column)

--- tabadol/posts/routes.py
+++ tabadol/posts/routes.py
@@ -12,15 +12,24 @@ posts = Blueprint('posts', __name__)
 @posts.route("/post/new", methods=['GET', 'POST'])
 @login_required
 def new_post():
+    user = current_user.user
+    for item in user:
+        user_id = item.id
     form = PostForm()
+    form.offer_type.choices = [(item.id, item.name)
+                               for item in OfferType.query.all()]
     if form.validate_on_submit():
         post = Post(title=form.title.data,
-                    content=form.content.data, author=current_user)
+                    content=form.content.data, author=current_user, address=form.address.data,
+                    location=Post.point_representation(
+                        form.lat.data, form.lng.data),
+                    offer_type_id=form.offer_type.data, user_id=user_id)
         db.session.add(post)
         db.session.commit()
         flash('Your offer has been successfully created!', 'success')
         return redirect(url_for('main.home'))
-    return render_template('create_post.html', title='New Offer', form=form, legend='New Post')
+    return render_template('create_post.html', title='New Offer', form=form,  user=user,
+                           map_key=app.config['API_KEY'], legend='New Offer')
 
 
 @posts.route("/post/<int:post_id>")
@@ -39,14 +48,18 @@ def update_post(post_id):
     if form.validate_on_submit():
         post.title = form.title.data
         post.content = form.content.data
+        post.street_address = form.street_address.data
+        post.location = Post.point_representation(
+            form.lat.data, form.lng.data),
         db.session.commit()
         flash('The offer has been updated!', 'success')
         return redirect(url_for('posts.post', post_id=post.id))
     elif request.method == 'GET':
         form.title.data = post.title
         form.content.data = post.content
+        form.street_address.data = post.street_address
     return render_template('create_post.html', title='Update Post',
-                           form=form, legend='Update Post')
+                           form=form, legend='Update Post', post=post)
 
 
 @posts.route("/post/<int:post_id>/delete", methods=['POST'])
@@ -57,5 +70,25 @@ def delete_post(post_id):
         abort(403)
     db.session.delete(post)
     db.session.commit()
-    flash('Your post has been deleted!', 'success')
+    flash('Your offer has been deleted!', 'success')
     return redirect(url_for('main.home'))
+
+
+# JSON API route
+# here i can set a route to create an api url
+# within the function i will create a dictionary using the db data and return this dictionary as json (jsonify)
+
+@posts.route('/api/get_offers')
+def api_all():
+   # here the lat, lng, and radius is called from the API url created in js file
+    lat = float(request.args.get('lat'))
+    radius = int(request.args.get('radius'))
+ # explanation of request.args.get():
+ # https://www.digitalocean.com/community/tutorials/processing-incoming-request-data-in-flask
+
+    posts = Post.get_offers_within_radius(lat=lat, lng=lng, radius=radius)
+    # posts = Post.query.all()
+    output = []
+    for item in posts:
+        output.append(item.to_dict())
+    return jsonify(output)
