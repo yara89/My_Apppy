afcb42894245a410615672f5422db99b98c85e88
yara89 <60362934+yara89@users.noreply.github.com>
2021-03-03 09:50:38 +0100

    Update forms.py
    connect to Spatialite terminal error (no such column)

--- tabadol/posts/forms.py
+++ tabadol/posts/forms.py
@@ -1,9 +1,19 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, TextAreaField
+from wtforms import StringField, SubmitField, TextAreaField, SelectField, HiddenField
 from wtforms.validators import DataRequired
 
 
 class PostForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired()])
     content = TextAreaField('Content', validators=[DataRequired()])
+    lng = HiddenField('lng', validators=[DataRequired()])
+    lat = HiddenField('lat', validators=[DataRequired()])
+    location = HiddenField('location', validators=[DataRequired()])
+    offers_type = SelectField(u'Type of activity',
+                              choices=[('give',
+                                        'need')],
+                              validators=[
+                                  DataRequired()
+                              ])
+
     submit = SubmitField('Post')
